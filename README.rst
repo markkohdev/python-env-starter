d02654946d09fb298a74347a7f69f02720165700
Mark Koh <markkohdev@gmail.com>
2017-05-15 16:53:13 -0600

    Prep for packaging.

--- README.rst
+++ README.rst
@@ -1,30 +1,36 @@
-# Python Virtual Env Starter
+Python Env Starter
+===============================
 Getting your python repository setup for other developers to work on can be tedious and time consuming.  Use this handy
 utility to make setting up your python projects "mad easy" for other developers.
 
 Tons of thanks to @kennethreitz and @dasJ for making [autoenv](https://github.com/kennethreitz/autoenv) :)
 
-### What does it do??
+What does it do??
+---------------------------
 This script will create a few essential files in your repository:
 
-##### .env
+**.env**
 This file is used by autoenv to automatically switch to your python virtualenv and run any generic shell commands
 (such as modifying your $PATH or $BIN, adding aliases, etc.) whenever you `cd` into your project directory.
 
-##### setup.sh
+**setup.sh**
 This file allows developers who clone your repository to easily install OS-level dependencies.
 
-##### .gitignore
+**.gitignore**
 This will add or update your gitignore to include the virtual env and compiled python files.  You don't wanna source
 control that!
 
-### How do I do it??
+How do I do it??
+----------------------
 It's easy peasy!  Just run
-```
-sudo pip install python-env
-python-env setup
-./setup.sh
-```
-You should now see two new files, `.env` and `setup.sh`
 
-then commit your changes to your own repo :)
+.. code:: bash
+
+    sudo pip install python-env
+    python-env setup
+    ./setup.sh
+
+
+You should now see the new files, ``.env``, ``setup.sh``, and ``.gitignore``
+
+then commit the changes to your own repo :)
---
+++ setup.py
@@ -0,0 +1,119 @@
+"""The setup script for python-env-starter
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='python-env-starter',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1',
+
+    description='A tool for getting setup with a common python environment',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/markkohdev/python-env-starter',
+
+    # Author details
+    author='Mark Koh',
+    author_email='markkohdev@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='samname='python-env-starter',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1',
+
+    description='A tool for getting setup with a common python environment',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/markkohdev/python-env',
+
+    # Author details
+    author='Mark Koh',
+    author_email='markkohdev@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='env environment autoenv setuptools development',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['Jinja2==2.9.6']
+)
